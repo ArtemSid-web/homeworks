ab488f1a26fc29b7e26b1af25041753657bceaf7
Artem <sidarvit@yandex.ru>
2025-03-06 01:16:37 +0300

    HW_2_1

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -16,15 +16,32 @@ DATA = {
         'сыр, ломтик': 1,
         'помидор, ломтик': 1,
     },
-    # можете добавить свои рецепты ;)
+    # можете добавить свои рецепты
 }
 
 # Напишите ваш обработчик. Используйте DATA как источник данных
 # Результат - render(request, 'calculator/index.html', context)
 # В качестве контекста должен быть передан словарь с рецептом:
+
 # context = {
 #   'recipe': {
 #     'ингредиент1': количество1,
 #     'ингредиент2': количество2,
 #   }
 # }
+
+
+def rechelp(request, dish):
+    servings = int(request.GET.get('servings', 1))
+    recipe = DATA[dish]
+    if servings > 1 and servings != 0:
+        r_1 = {k: v * servings for k, v in recipe.items()}
+        context = {
+            'recipe': r_1
+        }
+    else:
+        context = {
+            'recipe': recipe
+        }
+
+    return render(request, 'calculator/index.html', context)
